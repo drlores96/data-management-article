39578059346a5b049b318251017e74204c46dacc
drlores <david.lores@sdggroup.com>
2024-02-16 10:07:31 +0100

    added validation

--- src/data_manager/validator.py
+++ src/data_manager/validator.py
@@ -1,5 +1,6 @@
+from typing import Optional
 import pandas as pd
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 
 
 class Schema(BaseModel):
@@ -9,7 +10,15 @@ class Schema(BaseModel):
     def check(cls, data: pd.DataFrame) -> bool:
         data_dict = data.to_dict(orient="records")
         try:
-            [cls.model_validate(**dictionary) for dictionary in data_dict]
+            [cls.model_validate(dictionary) for dictionary in data_dict]
             return True
         except:
             return False
+
+
+class Iris(Schema):
+    sepal_length: Optional[float] = Field(gt=0)
+    sepal_width: Optional[float] = Field(gt=0)
+    petal_length: Optional[float] = Field(gt=0)
+    petal_width: Optional[float] = Field(gt=0)
+    species: Optional[str] = Field(max_length=10)
